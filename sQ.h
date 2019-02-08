7811d33ace03fb0416d284279f6e0e51750e43d8
Lawrence Scroggs <lscroggs@augusta.cs.pdx.edu>
2019-02-08 15:46:54 -0800

    Just Starting

---
+++ sQ.h
@@ -0,0 +1,48 @@
+// This is the header file I will implement my classes and struct
+
+
+# include <cctype>
+# include <iostream>
+# include <cstring>
+
+using namespace std;
+
+
+
+
+struct email{
+
+  char * user;
+  char * topic;
+  char * body;
+  char * date;
+
+
+};
+class stack{
+
+  public:
+    stack();
+    getInfo();
+    peek();
+    display();
+
+    
+
+  private:
+    peek_aboo();
+    display_private();
+    push(email * & head);
+    pop(email * & head);
+    
+
+
+};
+class queue{
+
+  public:
+
+  private:
+
+
+};
---
+++ stackQueue_Main.cpp
@@ -0,0 +1,25 @@
+// This program will use two different data structures to 
+// keep track of emails received from the user.  The first 
+// data structure will be a linear linked list of arrays.  
+// The second data structure will be a queue style circular
+// linked list.
+
+
+
+# include "sQ.h"
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
